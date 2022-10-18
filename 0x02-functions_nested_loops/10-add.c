ae86e64ce8b2466e899efa154a33d6b9af7df6bb
root <chwakik@gmail.com>
2022-10-18 12:23:55 -0700

    burning midnight candle

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+
+ * add - additional function
+
+ * @a: first input
+
+ * @b: second input
+
+ *
+
+ * Return: sum of both inputs
+
+ */
+
+
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
