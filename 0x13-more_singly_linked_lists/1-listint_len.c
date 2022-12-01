aaf1dac45f7d0cb1349893f089ee0674dfdabc28
root <chwakik@gmail.com>
2022-12-01 04:35:40 -0800

    setstring

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
+
+
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: pointer to the 1st node of the linked list
- * Return: number of nodes
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t nodenum = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
-		nodenum++;
-		
+		nnodes++;
 	}
-	return (nodenum);
+	return (nnodes);
 }
